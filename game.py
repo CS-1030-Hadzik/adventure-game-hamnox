3fe5681a181191191104f89e777629d42b2632d6
Mel Heisey <mheisey.nox@gmail.com>
2026-02-05 12:10:20 -0700

    refactor functions, add path, add inventory

--- game.py
+++ game.py
@@ -7,32 +7,86 @@ Description: This is a text-based adventure game where the player makes choices
 to navigate through a mysterious forest.
 """
 # Deviations from the example code have been taken where I feel appropriate.
+inventory = []
+
+# welcome player
+def welcome_player():
+    # Ask for the player's name.
+    name = input("What is your name adventurer? ")
+
+    # Welcome with personalized message.
+    print(f"Welcome, {name}! Your journey begins now.")
+    return name
+
+
+def describe_area():
+    # Describe the starting area.
+    starting_area = """
+    You find yourself in a dark forest.
+    The sound of rustling leaves fills the air.
+    Two paths lie ahead, leading deeper into the unknown...
+        1. Take the left path into the dark woods.
+        2. Take the right path toward the mountain pass
+        3. Stay where you are.
+        Type 'i' to view your inventory.
+    """
+    print(starting_area)
+
+def add_to_inventory(item):
+    inventory.append(item)
+    print(f"You picked up a {item}!")
+
+def print_inventory():
+    print(f"Items: {", ".join(inventory)}")
+
+def first_decision():
+    decision = input("What would you like to do? (1, 2, 3, or i): ").lower() # Normalize the answer.
+    if decision == "1":
+        print(f"Brave choice, {player_name}! You grab a lantern and venture forward into the dark woods.")
+        add_to_inventory("lantern")
+        print("")
+        return 1
+    elif decision == "2":
+        print(f"Clever choice {player_name}! You grab a map and venture forward into the mountains.")
+        add_to_inventory("map")
+        print("")
+        return 2
+    elif decision == "3":
+        print(player_name + ", you decide to wait. Perhaps courage will find you later.") # concatenation example
+        print("")
+    else:
+        handle_other_moves(decision)
+    return 0
+
+def handle_other_moves(move):
+    if move == "i":
+        print_inventory()
+    # TODO: handle quit
+    else:
+        print("Confused, you stand still, unsure of what to do.")
+
+
 
 # Welcome and introduction messsage.
-print("Welcome to the Adventure Game!")
-print("Your journey begins here...")
+print("""Welcome to the Adventure Game!
+Your journey begins here...
+""")
 
-# Ask for the player's name.
-player_name = input("What is your name adventurer? ")
-
-# Interpolate strings to create a personalized message.
-print(f"Welcome, {player_name}! Your journey begins now.")
-
-# Describe the starting area.
-starting_area = """
-You find yourself in a dark forest.
-The sound of rustling leaves fills the air.
-A faint path lies ahead, leading deeper into the unknown...
-"""
-print(starting_area)
+player_name = welcome_player()
+describe_area()
 
 # Ask the player for their first decision.
-decision = input("Do you wish to take the path? (yes or no): ").lower() # Normalize the answer.
+location = 0
+while location == 0:
+    location = first_decision()
 
-# TODO: replace with a while loop.
-if decision == "yes":
-    print(f"Brave choice, {player_name}! You step onto the path and venture forward.")
-elif decision == "no":
-    print(player_name + ", you decide to wait. Perhaps courage will find you later.") # concatenation example
-else:
-    print("Confused, you stand still, unsure of what to do.")
+# Allow player to check inventory
+next_action = input("What would you like to do? (i): ").lower()
+if next_action == "i":
+    print_inventory()
+
+
+# TODO: separate error and inventory handling
+# TODO: location continuations
+
+
